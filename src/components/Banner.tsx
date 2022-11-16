4623b65bb03221cd1b4df7c87df2ec646bd97751
mrt-stl <martin@stadtteilliebe.de>
2022-11-16 20:11:57 +0100

    update versions

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,10 +1,14 @@
 import React from "react"
 
-function Banner() {
+type Props = {
+    content?: string
+}
+
+function Banner({ content = "empty string" }: Props) {
     return (
         <div className='App'>
             <header className='App-header'>
-                <h1>COOKIE CONSENT</h1>
+                <h1>COOKIE CONSENT - {content}</h1>
             </header>
         </div>
     )
