f8ea020c118af87e277c646b24bc96f75ee920b6
mrt-stl <martin@stadtteilliebe.de>
2022-11-16 19:50:39 +0100

    init

---
+++ src/components/Banner.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Banner() {
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <h1>COOKIE CONSENT</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Banner
